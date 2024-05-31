e7cb45fb838a0f9af4a8417ca4fde5c94f18a973
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2024-05-31 13:09:30 -0300

    kt

--- tecnologiasMoveis/Kotlin/aula04_2705/appProdutosTDE/app/build.gradle.kts
+++ tecnologiasMoveis/Kotlin/aula04_2705/appProdutosTDE/app/build.gradle.kts
@@ -37,6 +37,10 @@ android {
 
 dependencies {
 
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
     // VielModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
--- tecnologiasMoveis/Kotlin/aula04_2705/appProdutosTDE/app/src/main/java/com/example/appprodutostde/MainActivity.kt
+++ tecnologiasMoveis/Kotlin/aula04_2705/appProdutosTDE/app/src/main/java/com/example/appprodutostde/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.appprodutostde
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,12 +24,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recycler = findViewById<RecyclerView>(R.id.recycler)
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        val menuBottom = findViewById<BottomNavigationMenuView>(R.id.menu_bottom)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.apply {
+            title = "home"
+            setDisplayHomeAsUpEnabled(true)
+        }
+
+        NavigationUI.setupWithNavController(menuBottom, navController)
+
+
+//        val recycler = findViewById<RecyclerView>(R.id.recycler)
 
         val listOfProducts = mainViewModel.getProducts()
         val adapter = ProductAdapter(listOfProducts, ::goToDetail)
 
-        recycler.adapter = adapter
+//        recycler.adapter = adapter
         }
 
     private fun goToDetail(item: Product) {
@@ -33,6 +51,11 @@ class MainActivity : AppCompatActivity() {
             putExtra("data", item)
             startActivity(this)
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 
 }
